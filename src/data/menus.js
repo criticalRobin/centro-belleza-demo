1fe4c72b5728847e1789610730cd5dfa180229f2
criticalRobin <matiastite1@gmail.com>
2025-02-13 09:35:52 -0500

    Centro de belleza demo

--- src/data/menus.js
+++ src/data/menus.js
@@ -1,60 +1,56 @@
 export const menuMain = [
-    {
-        name: "Home",
-        url: "/",
-        highlighted: false,
-    },
-    {
-        name: "Menu",
-        url: "/menu",
-        highlighted: false,
-    },
-    {
-        name: "About Us",
-        url: "/about-us",
-        highlighted: false,
-    },
-    {
-        name: "Gallery",
-        url: "/gallery",
-        highlighted: false,
-    },
-    {
-        name: "Contact",
-        url: "/contact",
-        highlighted: false,
-    },
-    {
-        name: "Book",
-        url: "/book",
-        highlighted: true,
-    },
+  {
+    name: "Inicio",
+    url: "/",
+    highlighted: false,
+  },
+  {
+    name: "Nosotros",
+    url: "/",
+    highlighted: false,
+  },
+  {
+    name: "Galería",
+    url: "/",
+    highlighted: false,
+  },
+  {
+    name: "Servicios",
+    url: "/",
+    highlighted: false,
+  },
+  {
+    name: "Contacto",
+    url: "/",
+    highlighted: false,
+  },
+  {
+    name: "Reservar",
+    url: "/",
+    highlighted: true,
+  },
 ];
 
 export const footerPrimary = [
-    {
-        title: "Navigatie",
-        items: [
-            { name: "home", url: "/" },
-        ],
-    },
+  {
+    title: "Navigatie",
+    items: [{ name: "home", url: "/" }],
+  },
 ];
 
 export const footerSecondary = [
-    {
-        title: "support",
-        items: [
-            { name: "pricing", url: "/pricing" },
-        ],
-    },
+  {
+    title: "support",
+    items: [{ name: "pricing", url: "/pricing" }],
+  },
 ];
 
 export const footerTertiary = [
-    {
-        title: "Links",
-        items: [
-            { name: "privacybeleid", url: '/privacy-policy' },
-            { name: "api status", url: "/api-status" },
-        ],
-    },
+  {
+    title: "Links",
+    items: [
+      { name: "privacybeleid", url: "/privacy-policy" },
+      { name: "api status", url: "/api-status" },
+    ],
+  },
 ];
