52f67277f4d36b8e89f47f3d0cfef301a144ad85
criticalRobin <matiastite1@gmail.com>
2025-02-13 09:57:23 -0500

    Modificados paquetes

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,22 +11,23 @@ import playformInline from "@playform/inline";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astropie.netlify.app",
-	base: "/",
-	// trailingSlash: 'always',
-	integrations: [
-		tailwind(),
-		alpinejs(),
-		mdx(),
-		(await import("@playform/inline")).default({
-			Critters: true,
-		}),
-	],
-	output: "static",
-	devToolbar: {
-		enabled: false,
-	},
-	experimental: {
-		svg: true,
-	},
+  site: "https://astropie.netlify.app",
+  base: "/",
+  // trailingSlash: 'always',
+  integrations: [
+    tailwind(),
+    alpinejs(),
+    mdx(),
+    (await import("@playform/inline")).default({
+      Critters: true,
+    }),
+  ],
+  output: "static",
+  devToolbar: {
+    enabled: false,
+  },
+  experimental: {
+    svg: true,
+    assets: true,
+  },
 });
